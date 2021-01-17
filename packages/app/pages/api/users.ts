deeeb2465de4cf9d74fd15e00d55c4f46d32cdb5
Ilya Daraseliya <idarase@gmail.com>
2021-01-17 20:14:15 +0100

    fix

--- packages/app/pages/api/users.ts
+++
@@ -1,9 +0,0 @@
-import {prisma} from '@feedback-loop-inc/database'
-
-export default async function handler(req, res) {
-    const users = await prisma.user.findMany()
-
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify(users))
-}
